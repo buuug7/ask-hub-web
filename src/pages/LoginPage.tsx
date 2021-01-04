8f5130275f2d0155f10dd0ef621e400389540acf
buuug7 <youpp@126.com>
2021-01-04 22:06:52 +0800

    update LoginPage and update bootstrap-icons dependency

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,6 +3,8 @@ import { http } from "../http";
 import { useHistory } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { userState } from "../app.state";
+import "./LoginPage.scss";
+import { ReactComponent as GithubIcon } from "bootstrap-icons/icons/github.svg";
 
 function LoginPage() {
   const history = useHistory();
@@ -27,47 +29,71 @@ function LoginPage() {
 
   return (
     <div className="LoginPage mt-4">
-      <h2>Login to Askhub</h2>
-      <form className="mt-4">
-        <div className="mb-3">
-          <label htmlFor="email" className="formLabel">
-            Email
-          </label>
-          <input
-            type="text"
-            className="formControl"
-            value={email}
-            id="email"
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
+      <div className="login-wrap">
+        <h4>登录 Askhub</h4>
+        <form className="mt-4">
+          <div className="mb-3">
+            <label htmlFor="email" className="formLabel">
+              邮箱
+            </label>
+            <input
+              type="text"
+              className="formControl"
+              value={email}
+              id="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label htmlFor="password" className="formLabel">
-            password
-          </label>
-          <input
-            className="formControl"
-            type="text"
-            value={password}
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="formLabel">
+              密码
+            </label>
 
-        <button
-          className="btn primary"
-          type="submit"
-          onClick={async (e) => {
-            e.preventDefault();
-            await login();
-            await getUserInfo();
-          }}
-        >
-          login
-        </button>
-      </form>
+            <input
+              className="formControl"
+              type="text"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <a href="#" className="tips">
+              忘记密码？
+            </a>
+          </div>
+
+          <div>
+            <button
+              className="btn primary"
+              type="submit"
+              onClick={async (e) => {
+                e.preventDefault();
+                await login();
+                await getUserInfo();
+              }}
+            >
+              登录
+            </button>
+
+            <button className="btn ml-2">注册</button>
+          </div>
+        </form>
+
+        <hr/>
+        <div className="mt-4">
+          <p>通过社交账号登录</p>
+          <div>
+            <button className="btn mr-2">
+              <GithubIcon style={{ fontSize: "2rem" }} />
+            </button>
+            <button className="btn">微信</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
